82d1ac95dd0b974c4adc446873e2899e0e52e66b
Wan Muzaffar <wanmuz86@gmail.com>
2021-06-10 10:23:46 +0800

    add item

--- lib/widgets/detail_pass_back.dart
+++ lib/widgets/detail_pass_back.dart
@@ -6,12 +6,32 @@ class DetailPassBackPage extends StatefulWidget {
 }
 
 class _DetailPassBackPageState extends State<DetailPassBackPage> {
+
+  var dataTextEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Detail Page pass back"),),
-        body:Center(
-          child: Text("This is detail page with pass back"),
+        body:Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                TextField(
+                  controller: dataTextEditingController,
+                  decoration: InputDecoration(
+                    hintText: "Enter data to pass back"
+                  ),
+                ),
+                TextButton(onPressed: (){
+                  // Code to close a particular page
+                  // The second paramater is optional, if you want to pass back data
+                  Navigator.pop(context, dataTextEditingController.text);
+
+                }, child: Text("Pass back data"))
+              ],
+            )
+          ),
         )
     );
   }
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_nav/widgets/detail.dart';
 import 'package:flutter_nav/widgets/detail_data.dart';
-
+import 'package:flutter_nav/widgets/detail_pass_back.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -11,39 +12,58 @@ class _HomePageState extends State<HomePage> {
   // 2) Create a controller (TextEditingController) where you will link the form to this controller
 
   var _dataController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Navigation Demo"),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => DetailPage()));
-                },
-                child: Text("Go to detail page")),
-            TextField(
-              controller: _dataController,
-              decoration: InputDecoration(hintText:
-              "Enter data to be passed to second page"),
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => DetailDataPage(newData:
-                    _dataController.text))
-                  );
-                  
-                }, child: Text("Go to detail page with data")),
-            TextButton(
-                onPressed: () {},
-                child: Text("Go to detail page pass back data"))
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => DetailPage()));
+                  },
+                  child: Text("Go to detail page")),
+              TextField(
+                controller: _dataController,
+                decoration: InputDecoration(
+                    hintText:
+                "Enter data to be passed to second page"),
+                keyboardType: TextInputType.text
+              ),
+              TextButton(
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DetailDataPage(newData:
+                      _dataController.text))
+                    );
+
+                  }, child: Text("Go to detail page with data")),
+              TextButton(
+                  onPressed: () async {
+                    FocusScope.of(context).unfocus();
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context)=>DetailPassBackPage())
+                    );
+                    Fluttertoast.showToast(
+                        msg: result,
+                        toastLength: Toast.LENGTH_LONG,
+                        gravity: ToastGravity.BOTTOM
+                    );
+                  },
+                  child: Text("Go to detail page pass back data"))
+            ],
+          ),
         ),
       ),
     );
